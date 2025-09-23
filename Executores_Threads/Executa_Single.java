6eb71d619f64957043ff3179078dc42411180348
cibelevs <cibelevale.estudante@gmail.com>
2025-09-23 07:10:27 -0300

    add meuCallable

--- Executores_Threads/Executa_Single.java
+++ Executores_Threads/Executa_Single.java
@@ -1,6 +1,7 @@
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Executa_Single{
     public static void main(String[] args) throws InterruptedException {
@@ -8,7 +9,13 @@ public class Executa_Single{
         try {
             executor = Executors.newSingleThreadExecutor();
             executor.execute(new MeuRunnable());
-            executor.awaitTermination(5, java.util.concurrent.TimeUnit.SECONDS);
+            executor.execute(new MeuRunnable());
+            executor.execute(new MeuRunnable());
+            Future<?> future = executor.submit(new MeuRunnable());
+            System.out.println(future.isDone());
+            executor.shutdown();
+            executor.awaitTermination(10, java.util.concurrent.TimeUnit.SECONDS);
+            System.out.println(future.isDone());
         }      
          catch (Exception e) {
             throw e;
@@ -27,6 +34,17 @@ public class Executa_Single{
             System.out.println("Nome da thread: " + nome);
         }
     }
+
+   /** * public static class MeuCallabe implements Callable<String> {
+        @Override
+        public String call() throws Exception { 
+            System.out.println("Executando a tarefa em uma thread separada.");
+            String nome = Thread.currentThread().getName();
+            System.out.println("Nome da thread: " + nome);
+            return nome;
+        }
+    } 
+    **/
 }
 
     
