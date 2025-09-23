1d2a4581a2593bfd8ba7cbfee4da1d25721e748a
cibelevs <cibelevale.estudante@gmail.com>
2025-09-23 07:19:26 -0300

    separando pastas

---
+++ Executores_Threads/Multi/MultiThreads.java
@@ -0,0 +1,8 @@
+package Executores_Threads.Multi;
+
+public class MultiThreads {
+    public static void main(String[] args) {
+        System.out.println("Implementação de múltiplas threads.");
+        
+    }
+}
