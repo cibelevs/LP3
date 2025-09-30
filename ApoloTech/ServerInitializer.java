176001886f3dc1dfb86c9be44c8aa039966c0d7b
cibelevs <cibelevale.estudante@gmail.com>
2025-09-30 07:53:18 -0300

    atv em sala (funcionou)

--- ApoloTech/ServerInitializer.java
+++ ApoloTech/ServerInitializer.java
@@ -1,32 +1,34 @@
 package ApoloTech;
-//**A Apolo Tech é responsável por um microsserviço crítico de autenticação. Para garantir
-    //a segurança e o desempenho, o microsserviço principal (AuthService) só pode se liga
 import java.util.concurrent.CountDownLatch;
 
-    //porta (SocketBind) e começar a aceitar requisições externas depois que seus quatro
-    //módulos essenciais de inicialização forem completamente carregados. Se o servidor
-    //iniciar antes da conclusão de qualquer um desses módulos (Configuração, Cache,
-    //Chaves de Criptografia, e Conexão de Log), ele pode falhar ou expor vulnerabilidades de
-    //segurança. Sabendo que o módulo de configuração leva em média 6 segundos para ser
-    //carregado, o de cache leva em média 9 segundos, o de chaves de criptografia em média
-    //12 segundos e o da conexão de log em média 4 segundos.
-    //A Apolo Tech precisa de uma solução robusta e concorrente para gerenciar essa
-    //dependência. Desta forma ela te contratou com o intuito de solucionar o seu problema,
-    //para isso você irá implementar um sistema de inicialização de servidor que só inicia a
-    //aceitação de conexões (simulada) depois que todos os módulos de configuração e
-    //segurança forem carregados.
-    //Requisitos
-    //1. Crie uma classe ServerInitializer que:
-    //a. Contém uma instância de CountDownLatch inicializada com o valor 4.
-    //b. Possui um método waitForInitialization() que chama latch.await().
-    //c. Possui um método startServer() que imprime a mensagem “Servidor
-    //Principal Online: Pronto para aceitar conexões (Socket.bind())” após o
-  //  await() ser liberado.
+/**A Apolo Tech é responsável por um microsserviço crítico de autenticação. Para garantir
+    a segurança e o desempenho, o microsserviço principal (AuthService) só pode se liga
 
-//
+    porta (SocketBind) e começar a aceitar requisições externas depois que seus quatro
+    módulos essenciais de inicialização forem completamente carregados. Se o servidor
+    iniciar antes da conclusão de qualquer um desses módulos (Configuração, Cache,
+    Chaves de Criptografia, e Conexão de Log), ele pode falhar ou expor vulnerabilidades de
+    segurança. Sabendo que o módulo de configuração leva em média 6 segundos para ser
+    carregado, o de cache leva em média 9 segundos, o de chaves de criptografia em média
+    12 segundos e o da conexão de log em média 4 segundos.
+    A Apolo Tech precisa de uma solução robusta e concorrente para gerenciar essa
+    dependência. Desta forma ela te contratou com o intuito de solucionar o seu problema,
+    para isso você irá implementar um sistema de inicialização de servidor que só inicia a
+    aceitação de conexões (simulada) depois que todos os módulos de configuração e
+    segurança forem carregados.
+    Requisitos
+    1. Crie uma classe ServerInitializer que:
+    a. Contém uma instância de CountDownLatch inicializada com o valor 4.
+    b. Possui um método waitForInitialization() que chama latch.await().
+    c. Possui um método startServer() que imprime a mensagem “Servidor
+    Principal Online: Pronto para aceitar conexões (Socket.bind())” após o
+    await() ser liberado.
+*/
+
 public class ServerInitializer {
     private static CountDownLatch latch = new CountDownLatch(4);
 
+    /* 
     public static void main(String[] args) {
 
        Thread configModule = new Thread(() -> {
@@ -41,7 +43,7 @@ public class ServerInitializer {
             }
         }); 
     }
-
+    */
     public void waitForInitialization() {
         try {
             latch.await(); // Aguarda até que todos os módulos sejam carregados
