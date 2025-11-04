cdd984af4e946e1b0c889708814fca2754dfb66e
cibelevs <cibelevale.estudante@gmail.com>
2025-11-04 07:58:14 -0300

    adding

--- un2/RMI/exSala/leilao/ServicoLeilao.java
+++ un2/RMI/exSala/leilao/ServicoLeilao.java
@@ -1,11 +1,15 @@
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 
-public interface ServicoLeilao {
 
-    public String receberNotificaçãoLancesMaiores(int idCadastro);
-    public Double consultarMaiorLance();
-    public String ofertarLances();
+
+public interface ServicoLeilao extends Remote{
+
+    public String receberNotificaçãoLancesMaiores(int idCadastro) throws RemoteException;
+    public Double consultarMaiorLance() throws RemoteException;
+    public String ofertarLances() throws RemoteException;;
     
     
 
